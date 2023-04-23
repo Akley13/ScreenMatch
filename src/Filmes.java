384254c457bc69b5a6466f22f9e047947a41529d
Akley Alves <akleyalvescr@gmail.com>
2023-04-23 20:45:16 -0300

    Update Filmes.java

--- src/Filmes.java
+++ src/Filmes.java
@@ -1,7 +1,9 @@
 public class Filmes {
     String nome;
     String anoDeLancamento;
+    String sinopse;
     boolean incluidoNoPlano;
     double avaliacao;
     int totalDeAvaliacoes;
+    int duracaoEmMinutos;
 }
