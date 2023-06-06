815b3a544ccfa976ebfba5e095ed86233176fe5f
Akley Alves <akleyalvescr@gmail.com>
2023-06-06 00:16:27 -0300

    Update GamesXbox.java

--- src/br/com/akley/screenmatch/modelos/GamesXbox.java
+++ src/br/com/akley/screenmatch/modelos/GamesXbox.java
@@ -1,6 +1,5 @@
 package br.com.akley.screenmatch.modelos;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 // CLASSE - tudo que a classe tem, suas caracteristicas e o que ela faz.
 public class GamesXbox extends Consoles { // Extends - Palavra reservada do Java que aplica HERANÇA à uma classe. Aqui estou dizendo que GamesXbox tem tudo que Consoles têm, mais essas diferenças a baixo.
