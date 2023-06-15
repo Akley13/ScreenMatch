bfe7b23e15dabb1e8c0de7033fffb0df385afe2d
Akley Alves <akleyalvescr@gmail.com>
2023-06-15 20:48:50 -0300

    Update CalculoHorasDeJogo.java

--- src/br/com/akley/screenmatch/sections/calculos/CalculoHorasDeJogo.java
+++ src/br/com/akley/screenmatch/sections/calculos/CalculoHorasDeJogo.java
@@ -1,8 +1,9 @@
 package br.com.akley.screenmatch.sections.calculos;
 
 import br.com.akley.screenmatch.sections.GamesPlaystation;
+import br.com.akley.screenmatch.sections.GamesXbox;
 
-public class HorasJogadas {
+public class CalculoHorasDeJogo {
     private int tempoTotal;
 
     public int getTempoTotal() {
@@ -10,6 +11,10 @@ public class HorasJogadas {
     }
 
     public void inclui(GamesPlaystation duracaoDoJogo) {
+        tempoTotal += duracaoDoJogo.getDuracaoEmHoras();
+    }
+
+    public void inclui(GamesXbox duracaoDoJogo) {
         tempoTotal += duracaoDoJogo.getDuracaoEmHoras();
     }
 }
